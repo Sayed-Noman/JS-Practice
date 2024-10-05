f1a5f0932de0143791f95ab16e5bfad6fd4fe082
Sayed-Noman <sayed.siddik@northsouth.edu>
2024-10-06 02:54:11 +0600

    # Added 15

--- Basics/15_Functions/argument_keywords_script.js
+++ Basics/15_Functions/argument_keywords_script.js
@@ -2,46 +2,45 @@
 
 // Arguments Keyword with function declaration
 function add(num1, num2) {
-    console.log(arguments) // Logging arguments
-    for (let i = 0; i < arguments.length; i++) { // Looping arguments
-      console.log(`argument[${i}] : ${arguments[i]}`)
-    }
-    return num1 + num2
+  console.log(arguments) // Logging arguments
+  for (let i = 0; i < arguments.length; i++) { // Looping arguments
+    console.log(`argument[${i}] : ${arguments[i]}`)
   }
-  add(10, 20)
-  add(10, 20, 30, 40)
-  
-  // Arguments Keyword with function expression
-  const sub = function (num1, num2) {
-    console.log(arguments) // Logging arguments
-    for (let i = 0; i < arguments.length; i++) { // Looping arguments
-      console.log(`argument[${i}] : ${arguments[i]}`)
-    }
-    return num1 - num2
+  return num1 + num2
+}
+add(10, 20)
+add(10, 20, 30, 40)
+
+// Arguments Keyword with function expression
+const sub = function (num1, num2) {
+  console.log(arguments) // Logging arguments
+  for (let i = 0; i < arguments.length; i++) { // Looping arguments
+    console.log(`argument[${i}] : ${arguments[i]}`)
   }
-  sub(10, 20)
-  sub(10, 20, 30, 40)
-  
-  // Arguments Keyword with arrow function
-  const mul = (num1, num2) => {
-    // console.log(arguments) // Logging arguments doesnt work with arrow function
-    // for (let i = 0; i < arguments.length; i++) { // Looping arguments doesnt work with arrow function
-    //   console.log(`argument[${i}] : ${arguments[i]}`)
-    // }
-    return num1 * num2
+  return num1 - num2
+}
+sub(10, 20)
+sub(10, 20, 30, 40)
+
+// Arguments Keyword with arrow function
+const mul = (num1, num2) => {
+  // console.log(arguments) // Logging arguments doesnt work with arrow function
+  // for (let i = 0; i < arguments.length; i++) { // Looping arguments doesnt work with arrow function
+  //   console.log(`argument[${i}] : ${arguments[i]}`)
+  // }
+  return num1 * num2
+}
+mul(10, 20)
+mul(10, 20, 30, 40)
+
+// Convert Arguments keyword into Array
+const div = function (num1, num2) {
+  let argumentsArray = []
+  for (let i = 0; i < arguments.length; i++) {
+    argumentsArray.push(arguments[i]);
   }
-  mul(10, 20)
-  mul(10, 20, 30, 40)
-  
-  // Convert Arguments keyword into Array
-  const div = function (num1, num2) {
-    let argumentsArray = []
-    for (let i = 0; i < arguments.length; i++) {
-      argumentsArray.push(arguments[i]);
-    }
-    console.log(argumentsArray)
-    return num1 / num2
-  }
-  div(10, 2)
-  div(10, 2, 30, 3)
-  
+  console.log(argumentsArray)
+  return num1 / num2
+}
+div(10, 2)
+div(10, 2, 30, 3)
