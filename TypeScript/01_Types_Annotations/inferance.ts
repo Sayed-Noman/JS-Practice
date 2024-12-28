a39fc9c8ca45cea07efd75416d23861d10213fef
Sayed-Noman <sayed.siddik@northsouth.edu>
2024-12-29 03:15:20 +0600

    #Added 00 - 01

---
+++ TypeScript/01_Types_Annotations/inferance.ts
@@ -0,0 +1,12 @@
+// Here we're inferring the types!
+let tech = "TypeScript";
+let favouriteNumber = 8;
+let tsIsHard = true;
+
+console.log(tech, favouriteNumber, tsIsHard);
+console.log(typeof tech, typeof favouriteNumber, typeof tsIsHard)
+
+// ERROR
+// tech = false
+// favouriteNumber = "Hello"
+// tsIsHard = 20
---
+++ TypeScript/01_Types_Annotations/string.ts
@@ -0,0 +1,8 @@
+// let myVar: type = value
+
+let myName: string = "Sayed";
+myName = "Noman";
+console.log(myName);
+
+// ERROR
+// myName = 12;
